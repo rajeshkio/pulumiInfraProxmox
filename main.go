a300d1989497381987be37bb52e6fd8753842fbf
Rajesh Kumar <rk90229@gmail.com>
2025-07-06 09:32:01 +0530

    adding Harvester changes to the infra

--- main.go
+++ main.go
@@ -28,22 +28,41 @@ type ActionContext struct {
 	Templates    VMTemplate
 }
 
+type IPXEConfig struct {
+	BootServerURL string   `yaml:"bootServerUrl"`
+	OSType        string   `yaml:"osType"`
+	Version       string   `yaml:"version"`
+	BaseURL       string   `yaml:"baseUrl,omitempty"`
+	ConfigUrl     string   `yaml:"configUrl,omitempty"`
+	KernelParams  []string `yaml:"kernelParams,omitempty"`
+	AutoInstall   bool     `yaml:"autoInstall,omitempty"`
+	ISOFileName   string   `yaml:"isoFileName,omitempty"`
+
+	KernelURL string `yaml:"kernelUrl,omitempty"`
+	InitrdURL string `yaml:"initrdUrl,omitempty"`
+	ISOURL    string `yaml:"isoUrl,omitempty"`
+}
 type VMTemplate struct {
-	Name       string   `yaml:"name"`
-	VMName     string   `yaml:"vmName"`
-	ID         int64    `yaml:"id"`
-	DiskSize   int64    `yaml:"disksize"`
-	Memory     int64    `yaml:"memory"`
-	CPU        int64    `yaml:"cpu"`
-	IPConfig   string   `yaml:"ipconfig"`
-	IPs        []string `yaml:"ips,omitempty"`
-	Gateway    string   `yaml:"gateway"`
-	Username   string   `yaml:"username"`
-	Password   string   `yaml:"password,omitempty"` // global password
-	AuthMethod string   `yaml:"authMethod"`
-	Count      int64    `yaml:"count,omitempty"`
-	Role       string   `yaml:"role,omitempty"` // NEW!
-	Actions    []Action `yaml:"actions,omitempty"`
+	Name        string   `yaml:"name"`
+	VMName      string   `yaml:"vmName"`
+	ID          int64    `yaml:"id"`
+	DiskSize    int64    `yaml:"disksize"`
+	Memory      int64    `yaml:"memory"`
+	CPU         int64    `yaml:"cpu"`
+	IPConfig    string   `yaml:"ipconfig"`
+	IPs         []string `yaml:"ips,omitempty"`
+	Gateway     string   `yaml:"gateway"`
+	Username    string   `yaml:"username"`
+	Password    string   `yaml:"password,omitempty"` // global password
+	AuthMethod  string   `yaml:"authMethod"`
+	Count       int64    `yaml:"count,omitempty"`
+	Role        string   `yaml:"role,omitempty"` // NEW!
+	Actions     []Action `yaml:"actions,omitempty"`
+	ProxmoxNode string   `yaml:"proxmoxNode,omitempty"`
+
+	// ipxe boot options for Harvester
+	BootMethod string      `yaml:"bootMethod,omitempty"`
+	IPXEConfig *IPXEConfig `yaml:"ipxeConfig,omitempty"`
 }
 
 type RoleGroup struct {
@@ -53,9 +72,10 @@ type RoleGroup struct {
 type ActionHandler func(ctx *pulumi.Context, actionctx ActionContext) error
 
 var actionHandlers = map[string]ActionHandler{
-	"install-haproxy":    handleInstallHAProxy,
-	"install-k3s-server": handleInstallK3Sserver,
-	"get-kubeconfig":     handleGetKubeconfig,
+	"install-haproxy":     handleInstallHAProxy,
+	"install-k3s-server":  handleInstallK3Sserver,
+	"get-kubeconfig":      handleGetKubeconfig,
+	"configure-ipxe-boot": handleConfigureIPXEBoot,
 }
 
 func handleInstallHAProxy(ctx *pulumi.Context, actionctx ActionContext) error {
@@ -70,11 +90,14 @@ func handleInstallHAProxy(ctx *pulumi.Context, actionctx ActionContext) error {
 	ctx.Log.Info(fmt.Sprintf("installing haproxy on %s with backends: %v", lbIP, k3sServerIPs), nil)
 	//	ctx.Log.Info(fmt.Sprintf("VM dependency: %v", lbVM.ID()), nil)
 
-	_, err := installHaProxy(ctx, lbIP, lbVM, k3sServerIPs)
+	cmd, err := installHaProxy(ctx, lbIP, lbVM, k3sServerIPs)
 	if err != nil {
 		ctx.Log.Error(fmt.Sprintf("HAProxy installation failed: %v", err), nil)
 	}
-	return err
+	actionctx.GlobalDeps["haproxy-install-command"] = cmd
+
+	ctx.Log.Info("HAProxy installation initiated successfully", nil)
+	return nil
 }
 
 func handleInstallK3Sserver(ctx *pulumi.Context, actionctx ActionContext) error {
@@ -83,6 +106,12 @@ func handleInstallK3Sserver(ctx *pulumi.Context, actionctx ActionContext) error 
 		return fmt.Errorf("k3s server needs loadbalancer IP but its not available")
 	}
 
+	var haproxyCmd pulumi.Resource
+	if haproxyResource, exists := actionctx.GlobalDeps["haproxy-install-command"]; exists {
+		if cmd, ok := haproxyResource.(*remote.Command); ok {
+			haproxyCmd = cmd
+		}
+	}
 	lbIP := lbIPs[0]
 	ctx.Log.Info(fmt.Sprintf("installing k3s server with LBIP: %s", lbIP), nil)
 	//var k3sCommands []*remote.Command
@@ -99,7 +128,7 @@ func handleInstallK3Sserver(ctx *pulumi.Context, actionctx ActionContext) error 
 			firstServerIP = serverIP
 			ctx.Log.Info(fmt.Sprintf("installing k3s on server %d: %s", i+1, serverIP), nil)
 
-			k3sCmd, err := installK3SServer(ctx, lbIP, actionctx.VMPassword, serverIP, serverVM, true, pulumi.String("").ToStringOutput(), nil)
+			k3sCmd, err := installK3SServer(ctx, lbIP, actionctx.VMPassword, serverIP, serverVM, true, pulumi.String("").ToStringOutput(), haproxyCmd)
 			if err != nil {
 				return fmt.Errorf("cannot install K3s server on first node %s: %w", serverIP, err)
 			}
@@ -110,7 +139,7 @@ func handleInstallK3Sserver(ctx *pulumi.Context, actionctx ActionContext) error 
 			}
 			k3sServerToken = tokenCmd.Stdout
 		} else {
-			_, err := installK3SServer(ctx, lbIP, actionctx.VMPassword, serverIP, serverVM, false, k3sServerToken, nil)
+			_, err := installK3SServer(ctx, lbIP, actionctx.VMPassword, serverIP, serverVM, false, k3sServerToken, haproxyCmd)
 			if err != nil {
 				return fmt.Errorf("cannot install k3s on server %s: %w", serverIP, serverVM)
 			}
@@ -121,6 +150,25 @@ func handleInstallK3Sserver(ctx *pulumi.Context, actionctx ActionContext) error 
 	actionctx.GlobalDeps["k3s-first-server-ip"] = firstServerIP
 	actionctx.GlobalDeps["k3s-loadbalancer-ip"] = lbIP
 	ctx.Log.Info(fmt.Sprintf("K3s installation initiated on %d servers", len(actionctx.VMs)), nil)
+	return nil
+}
+
+func handleConfigureIPXEBoot(ctx *pulumi.Context, actionCtx ActionContext) error {
+	template := actionCtx.Templates
+	config := template.IPXEConfig
+
+	if template.BootMethod != "ipxe" {
+		return fmt.Errorf("configure-ipxe-boot action requires bootMethod: ipxe")
+	}
+
+	// Simple approach: just log which script file the VM will use
+	scriptName := fmt.Sprintf("harvester-%s.ipxe", config.Version)
+	scriptURL := fmt.Sprintf("%s/boot/%s", config.BootServerURL, scriptName)
+
+	for _, vmIP := range actionCtx.IPs {
+		ctx.Log.Info(fmt.Sprintf("VM %s will boot using script: %s", vmIP, scriptURL), nil)
+	}
+
 	return nil
 }
 
@@ -146,11 +194,25 @@ func checkRequiredEnvVars() error {
 }
 
 func createVMFromTemplate(ctx *pulumi.Context, provider *proxmoxve.Provider, vmIndex int64, template VMTemplate, nodeName, gateway, password string) (*vm.VirtualMachine, error) {
-	var userAccount *vm.VirtualMachineInitializationUserAccountArgs
 
 	//	ctx.Log.Info(fmt.Sprintf("Creating VM with auth-method: %s, username: %s, password: %s", template.AuthMethod, template.Username, password), nil)
 	//	ctx.Log.Info(fmt.Sprintf("Template debug - Role: %s, AuthMethod: '%s', Username: %s", template.Role, template.AuthMethod, template.Username), nil)
 
+	ctx.Log.Info(fmt.Sprintf("Creating VM %s on node %s",
+		fmt.Sprintf("%s-%d", template.VMName, vmIndex), nodeName), nil)
+	switch template.BootMethod {
+	case "ipxe":
+		return createIPXEVM(ctx, provider, vmIndex, template, nodeName, gateway, password)
+	case "cloud-init":
+		return createCloudInitVM(ctx, provider, vmIndex, template, nodeName, gateway, password)
+	default:
+		return nil, fmt.Errorf("unsupported boot method: %s", template.BootMethod)
+	}
+}
+
+func createCloudInitVM(ctx *pulumi.Context, provider *proxmoxve.Provider, vmIndex int64, template VMTemplate, nodeName, gateway, password string) (*vm.VirtualMachine, error) {
+
+	var userAccount *vm.VirtualMachineInitializationUserAccountArgs
 	if template.AuthMethod == "ssh-key" {
 		sshKey := strings.TrimSpace(os.Getenv("SSH_PUBLIC_KEY"))
 		//		ctx.Log.Info(fmt.Sprintf("SSH KEY from env first 100 char: %s", sshKey[:100]), nil)
@@ -241,6 +303,65 @@ func createVMFromTemplate(ctx *pulumi.Context, provider *proxmoxve.Provider, vmI
 	return vmInstance, nil
 }
 
+func createIPXEVM(ctx *pulumi.Context, provider *proxmoxve.Provider, vmIndex int64, template VMTemplate, nodeName, gateway, password string) (*vm.VirtualMachine, error) {
+	if template.IPXEConfig == nil {
+		return nil, fmt.Errorf("iPXE boot method requires ipxeconfig")
+	}
+	vmName := fmt.Sprintf("%s-%d", template.VMName, vmIndex)
+
+	isoFileName := "harvester-ipxe.iso"
+	if template.IPXEConfig.ISOFileName != "" {
+		isoFileName = template.IPXEConfig.ISOFileName
+	}
+
+	//var ipConfig *vm.VirtualMachineInitializationIpConfigArray
+	vmInstance, err := vm.NewVirtualMachine(ctx, template.Name+fmt.Sprintf("-%d", vmIndex), &vm.VirtualMachineArgs{
+		Name:     pulumi.String(vmName),
+		NodeName: pulumi.String(nodeName),
+		Memory: &vm.VirtualMachineMemoryArgs{
+			Dedicated: pulumi.Int(template.Memory),
+		},
+		Cpu: &vm.VirtualMachineCpuArgs{
+			Cores: pulumi.Int(template.CPU),
+			Type:  pulumi.String("x86-64-v2-AES"),
+		},
+		BootOrders: pulumi.StringArray{
+			pulumi.String("scsi0"), // Disk first
+			pulumi.String("ide2"),  // Then CD-ROM with iPXE
+			pulumi.String("net0"),  // Then network
+		},
+		Disks: &vm.VirtualMachineDiskArray{
+			&vm.VirtualMachineDiskArgs{
+				Interface: pulumi.String("scsi0"),
+				//	DatastoreId: pulumi.String("nfs-iso"),
+				//	FileId:     pulumi.String("nfs-iso:iso/harvester-boot.iso"),
+				Size:       pulumi.Int(template.DiskSize), // Match your template's disk size
+				FileFormat: pulumi.String("raw"),
+				Iothread:   pulumi.Bool(true),
+			},
+		},
+		Cdrom: &vm.VirtualMachineCdromArgs{
+			Enabled:   pulumi.Bool(true),
+			FileId:    pulumi.String(fmt.Sprintf("nfs-iso:iso/%s", isoFileName)),
+			Interface: pulumi.String("ide2"),
+		},
+		NetworkDevices: &vm.VirtualMachineNetworkDeviceArray{
+			&vm.VirtualMachineNetworkDeviceArgs{
+				Bridge:   pulumi.String("vmbr0"),
+				Model:    pulumi.String("virtio"),
+				Firewall: pulumi.Bool(true),
+			},
+		},
+		Started: pulumi.Bool(true),
+		OnBoot:  pulumi.Bool(false),
+	}, pulumi.Provider(provider),
+		pulumi.DeleteBeforeReplace(true))
+	if err != nil {
+		return nil, err
+	}
+	return vmInstance, nil
+}
+
 func groupVMsByRole(allVMs []*vm.VirtualMachine, templates []VMTemplate) (map[string]RoleGroup, error) {
 	roleGroups := make(map[string]RoleGroup) // map with key string and value of type rolegroup
 	vmIndex := 0
@@ -289,6 +410,95 @@ func buildGlobalDependency(roleGroups map[string]RoleGroup) map[string]interface
 	}
 	return globalDeps
 }
+
+// func getDefaultBaseURL(osType string) string {
+// 	fmt.Println("DEBUG: from getDefaultBaseURL")
+// 	switch osType {
+// 	case "harvester":
+// 		return "https://releases.rancher.com/harvester"
+// 	case "ubuntu":
+// 		return "http://archive.ubuntu.com/ubuntu"
+// 	default:
+// 		return ""
+// 	}
+// }
+
+// func buildISOUrls(config *IPXEConfig) (string, string, string, error) {
+
+// 	fmt.Println("DEBUG: from buildISOUrls")
+// 	fmt.Printf("DEBUG: baseURL - 1%s", config.BaseURL)
+// 	if config.KernelURL != "" {
+// 		return config.KernelURL, config.InitrdURL, "", nil
+// 	}
+// 	if config.ISOURL != "" {
+// 		return "", config.ISOURL, "", nil
+// 	}
+// 	baseURL := config.BaseURL
+// 	if baseURL == "" {
+// 		baseURL = getDefaultBaseURL(config.OSType)
+// 	}
+// 	if baseURL == "" {
+// 		return "", "", "", fmt.Errorf("no base URL available for OS type: %s", config.OSType)
+// 	}
+// 	fmt.Printf("DEBUG: baseURL - 2%s", baseURL)
+// 	fmt.Printf("DEBUG: OSType=%s, Version=%s, BaseURL=%s\n", config.OSType, config.Version, baseURL)
+// 	switch config.OSType {
+// 	case "harvester":
+// 		return buildHarvesterURLs(baseURL, config.Version)
+// 	default:
+// 		return "", "", "", fmt.Errorf("unsupported OS type: %s", config.OSType)
+// 	}
+// }
+
+// func buildHarvesterURLs(baseUrl, version string) (string, string, string, error) {
+// 	fmt.Println("DEBUG: from buildHarvesterURLs")
+// 	if version == "" {
+// 		return "", "", "", fmt.Errorf("version required for Harvester")
+// 	}
+// 	base := fmt.Sprintf("%s/%s", baseUrl, version)
+// 	kernel := fmt.Sprintf("%s/harvester-%s-vmlinuz-amd64", base, version)
+// 	initrd := fmt.Sprintf("%s/harvester-%s-initrd-amd64", base, version)
+// 	rootfs := fmt.Sprintf("%s/harvester-%s-rootfs-amd64.squashfs", base, version)
+
+// 	fmt.Printf("DEBUG: kernel=%s, initrd=%s, rootfs=%s\n", kernel, initrd, rootfs)
+// 	return kernel, initrd, rootfs, nil
+// }
+// func generateIPXEScript(template VMTemplate, vmIP string) (string, error) {
+// 	fmt.Println("DEBUG: from generateIPXEScript")
+// 	config := template.IPXEConfig
+// 	if config == nil {
+// 		return "", fmt.Errorf("iPXE config required")
+// 	}
+// 	kernelUrl, initrdUrl, rootfsUrl, err := buildISOUrls(config)
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to build URLs: %w", err)
+// 	}
+
+// 	kernelParams := strings.Join(config.KernelParams, " ")
+// 	if config.ConfigUrl != "" {
+// 		kernelParams += fmt.Sprintf(" harvester.install.config_url=%s", config.ConfigUrl)
+// 	}
+// 	if rootfsUrl != "" {
+// 		kernelParams += fmt.Sprintf(" root=live:%s", rootfsUrl)
+// 	}
+// 	if config.AutoInstall {
+// 		kernelParams += " harvester.install.automatic=true"
+// 	}
+// 	script := fmt.Sprintf(`#!ipxe
+// 	dhcp
+// 	echo Network configured: ${net0/ip}
+// 	echo Downloading Harvester kernel...
+// kernel %s initrd=%s %s
+
+// echo Downloading initial ramdisk...
+// initrd %s
+
+// echo Starting Harvester installation...
+// boot
+// `, kernelUrl, filepath.Base(initrdUrl), kernelParams, initrdUrl)
+
+//		return script, nil
+//	}
 func installHaProxy(ctx *pulumi.Context, lbIP string, vmDependency pulumi.Resource, k3sServerIPs []string) (*remote.Command, error) {
 
 	var backendServers strings.Builder
@@ -398,6 +608,11 @@ EOF
 		`, lbIP, lbIP, k3sToken, lbIP)
 	}
 	resourceName := fmt.Sprintf("k3s-server-%s", strings.ReplaceAll(serverIP, ".", "-"))
+	dependencies := []pulumi.Resource{vmDependency}
+	if haproxyDependency != nil {
+		dependencies = append(dependencies, haproxyDependency)
+		ctx.Log.Info(fmt.Sprintf("K3s server %s will wait for HAProxy installation", serverIP), nil)
+	}
 	cmd, err := remote.NewCommand(ctx, resourceName, &remote.CommandArgs{
 		Connection: &remote.ConnectionArgs{
 			Host:     pulumi.String(serverIP),
@@ -405,7 +620,7 @@ EOF
 			Password: pulumi.String(vmPassword),
 		},
 		Create: k3sCommand,
-	}, pulumi.DependsOn([]pulumi.Resource{vmDependency}))
+	}, pulumi.DependsOn(dependencies))
 	return cmd, err
 }
 
@@ -447,17 +662,31 @@ func setupProxmoxProvider(ctx *pulumi.Context) (*proxmoxve.Provider, error) {
 	return provider, nil
 }
 
-func loadConfig(ctx *pulumi.Context) (string, string, string, []VMTemplate, error) {
+func loadConfig(ctx *pulumi.Context) (string, string, []VMTemplate, error) {
 	cfg := config.New(ctx, "")
-	proxmoxNode := cfg.Require("proxmox-node")
 	vmPassword := cfg.Require("password")
 	gateway := cfg.Require("gateway")
 
 	var templates []VMTemplate
 	cfg.RequireObject("vm-templates", &templates)
 
+	for i := range templates {
+		if templates[i].BootMethod == "" {
+			templates[i].BootMethod = "cloud-init"
+		}
+		if templates[i].AuthMethod == "" {
+			templates[i].AuthMethod = "password"
+		}
+		if templates[i].Username == "" {
+			templates[i].Username = "rajeshk"
+		}
+		if templates[i].ProxmoxNode == "" {
+			templates[i].ProxmoxNode = "proxmox-3"
+		}
+	}
+
 	ctx.Export("vmPassword", pulumi.String(vmPassword))
-	return proxmoxNode, vmPassword, gateway, templates, nil
+	return vmPassword, gateway, templates, nil
 
 }
 func executeAction(ctx *pulumi.Context, action Action, template VMTemplate, roleGroups RoleGroup, globalDeps map[string]interface{}, vmpassword string) error {
@@ -477,17 +706,52 @@ func executeAction(ctx *pulumi.Context, action Action, template VMTemplate, role
 	}
 }
 func executeActions(ctx *pulumi.Context, templates []VMTemplate, roleGroups map[string]RoleGroup, globalDeps map[string]interface{}, vmPassword string) error {
+
+	completedRoles := make(map[string]bool)
+
+	type ActionItem struct {
+		Template VMTemplate
+		Action   Action
+	}
+	var remainingActions []ActionItem
 	for _, template := range templates {
-		roleGroup := roleGroups[template.Role]
-
 		for _, action := range template.Actions {
-			ctx.Log.Info(fmt.Sprintf("Executing action '%s' for role '%s'", action.Type, template.Role), nil)
+			remainingActions = append(remainingActions, ActionItem{
+				Template: template,
+				Action:   action,
+			})
+		}
 
-			err := executeAction(ctx, action, template, roleGroup, globalDeps, vmPassword)
-			if err != nil {
-				return fmt.Errorf("failed to execute action %s for role %s: %w", action.Type, template.Role, err)
+		for len(remainingActions) > 0 {
+			executed := false
+
+			for i, item := range remainingActions {
+				canExecute := true
+				for _, dep := range item.Action.DependsOn {
+					if !completedRoles[dep] {
+						canExecute = false
+						break
+					}
+				}
+				if canExecute {
+					ctx.Log.Info(fmt.Sprintf("Executing action '%s' for role '%s'", item.Action.Type, item.Template.Role), nil)
+
+					roleGroup := roleGroups[item.Template.Role]
+					err := executeAction(ctx, item.Action, item.Template, roleGroup, globalDeps, vmPassword)
+					if err != nil {
+						return fmt.Errorf("failed to execute action %s for role %s: %w", item.Action.Type, item.Template.Role, err)
+					}
+					completedRoles[item.Template.Role] = true
+					remainingActions = append(remainingActions[:i], remainingActions[i+1:]...)
+					executed = true
+					break
+				}
+				if !executed {
+					return fmt.Errorf("dependency deadlock - remaining actions have unsatisfied dependencies")
+				}
 			}
 		}
+
 	}
 	return nil
 }
@@ -525,6 +789,7 @@ func handleGetKubeconfig(ctx *pulumi.Context, actionctx ActionContext) error {
 	lbIP := lbIPs[0]
 
 	ctx.Log.Info(fmt.Sprintf("Getting kubeconfig from %s with LB IP %s", firstServerIP, lbIP), nil)
+
 	cmd, err := getK3sKubeconfig(ctx, actionctx.Templates, firstServerIP, actionctx.VMPassword, lbIP, firstServerVM)
 	if err != nil {
 		return fmt.Errorf("failed to get kubeconfig: %w", err)
@@ -554,7 +819,7 @@ func main() {
 		}
 		fmt.Println(provider)
 
-		proxmoxNode, vmPassword, gateway, templates, err := loadConfig(ctx)
+		vmPassword, gateway, templates, err := loadConfig(ctx)
 		if err != nil {
 			return fmt.Errorf("failed to load config: %w", err)
 		}
@@ -565,7 +830,10 @@ func main() {
 			if count == 0 {
 				count = 1
 			}
-
+			proxmoxNode := template.ProxmoxNode
+			if proxmoxNode == "" {
+				proxmoxNode = "proxmox-3" // Use global default
+			}
 			for i := range count {
 				vm, err := createVMFromTemplate(ctx, provider, i, template, proxmoxNode, gateway, vmPassword)
 				if err != nil {
